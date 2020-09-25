31e8fb93beca86b361168f0f9fc2d39c1f3dcbb8
mihiran <154015E@uom.lk>
2020-09-25 15:17:23 +0530

    Resolve some git errors.Suceefullyget the product management interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {ProductListComponent} from './products/product-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   bootstrap: [AppComponent]
 })
